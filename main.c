c33e4939ce7fd4bc2ed20b718f192d3370eaa091
pxvxax <pxvxax@gmail.com>
2018-11-22 17:51:50 +0100

    najwiekszywspolnydzielnik

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void zamien(int *a, int *b) {
+    int tmp;
+    tmp = *a; *a = *b; *b = tmp;
+}
+
+int main() {
+    int a, b, c;
+
+    printf("Podaj a: ");  scanf("%d", &a);
+    printf("Podaj b: ");  scanf("%d", &b);
+
+    do{
+        if( a < b) zamien(&a, &b);
+        c = a - b;
+        a = b;
+        b = c;
+
+    }while(a != b);
+
+    printf("wynik: %d",  a);
+    return 0;
+}
